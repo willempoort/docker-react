61135ee0c3e3308621cdb4b17c83c31bb16bfef2
Willem M. Poort <willem@poort.net>
2021-01-12 20:57:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Zo ziet <code>code</code> er uit.
+          Somebody changed me.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.bien.nl"
           target="_blank"
           rel="noopener noreferrer"
         >
